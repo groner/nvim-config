acb21c035ccc47789f57efdd1403fd2567e1ef52
Kai Groner <kai@gronr.com>
2022-10-17 11:09:39 -0400

    Update cmp-nvim-lsp configuration.
    https://github.com/hrsh7th/cmp-nvim-lsp/issues/38

--- init.lua
+++ init.lua
@@ -328,8 +328,7 @@ local on_attach = function(_, bufnr)
 end
 
 -- nvim-cmp supports additional completion capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Make runtime files discoverable to the server
 local runtime_path = vim.split(package.path, ';')
