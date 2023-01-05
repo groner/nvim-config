d4e06e5682f33e395d5304273cd51e3f64a55edc
Kai Groner <kai@gronr.com>
2023-01-04 20:27:46 -0500

    Add cmp-nvim-lsp-signature-help.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -81,6 +81,7 @@ require('lazy').setup({
     -- Autocompletion plugin
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
     'saadparwaiz1/cmp_luasnip',
     -- Snippets plugin
     'L3MON4D3/LuaSnip',
