87ffe93ca0b3245b31ff68ef001bd64158641876
Kai Groner <kai@gronr.com>
2023-09-10 11:41:11 -0400

    Configure flex layout in telescope.

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>,",
+        function()
+          require("telescope.builtin").buffers({ sort_mru = true, show_all_buffers = true })
+        end,
+        desc = "Buffers",
+      },
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+    },
+
+    opts = {
+      defaults = {
+        layout_strategy = "flex",
+        layout_config = {
+          -- flex
+          flip_lines = 70,
+          flip_columns = 200,
+
+          horizontal = {
+            preview_width = 0.5,
+            preview_cutoff = 50,
+          },
+
+          vertical = {
+            preview_height = 0.5,
+            preview_cutoff = 15,
+          },
+        },
+      },
+    },
+  },
+}
